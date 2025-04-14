7a68b737e145f13635b2a91ecf42a335ee44a832
Wersplat <wersplat@gmail.com>
2025-04-13 22:21:35 -0400

    Clean up app.py by adding spacing for improved readability

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import sys
+print(sys.executable)
 
 from flask import Flask, render_template, g
 import sqlite3
 
+
 app = Flask(__name__)
 DATABASE = 'leaderboard.db'
+
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -11,11 +15,13 @@ def get_db():
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
+
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+
 
 @app.route('/')
 def leaderboard():
@@ -23,6 +29,7 @@ def leaderboard():
     cur.execute("SELECT team_name, wins, losses FROM teams ORDER BY wins DESC")
     rows = cur.fetchall()
     return render_template('leaderboard.html', rows=rows)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
