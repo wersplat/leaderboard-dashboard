d3ab1835dda0efc0fafbdb9d1c73ee177f4da7de
Wersplat <wersplat@users.noreply.github.com>
2025-04-14 13:20:30 -0400

    🐞 fix: correct debug flag to lowercase in app.run() for consistency

--- app.py
+++ app.py
@@ -272,6 +272,6 @@ def add_team():
 
 def new_func(__name__, app):
     if __name__ == "__main__":
-        app.run(host="0.0.0.0:", debug=True)
+        app.run(host="0.0.0.0:", debug=false)
 
 new_func(__name__, app)
